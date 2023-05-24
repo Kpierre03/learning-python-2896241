32f5302ee73019bad16f28da32a6bd3100d95288
Ken Phillip Pierre II <ken.pierre2@me.com>
2023-05-24 17:03:04 -0400

    Edited my first program in the Python Program.

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -2,5 +2,13 @@
 # Example file for HelloWorld
 # LinkedIn Learning Python course by Joe Marini
 #
+def main():  ## def is Defining a function and also helps create the structure of a function.  Line must be indented.
+    print("Hello World!")
+    name = input ("What is your name?")
+    print("Nice to meet you", name )
 
+
+if __name__=="__main__":
+    main()
+    
 
